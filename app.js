e483cea9e434c5bd98aa9d6cdb4b389682782fab
Cheng-Yuan Lai <a186235@gmail.com>
2023-03-21 23:28:58 +0800

    feat: handling POST request
    1. Enable users to add new tour into tours-simple.json
    2. Testing by Postman

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require('express');
 
 const app = express();
 
+// use middleware
+app.use(express.json());
+
 // app.get('/', (req, res) => {
 //   res
 //     .status(200)
@@ -25,6 +28,26 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+
+  const newId = tours[tours.length - 1].id + 1;
+  // merge two object
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          newTour,
+        },
+      });
+    }
+  );
+});
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
