0bf261452412b9627ff4b8b54b2e723a861536e8
Cheng-Yuan Lai <a186235@gmail.com>
2023-03-23 00:59:31 +0800

    feat: add checkBody middleware for POST

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,12 +3,19 @@ const tourController = require('./../controllers/tourControllers.js');
 const router = express.Router();
 
 // val parameter is hold value of the id parameter
+// par middleware
 router.param('id', tourController.checkID);
+
+// Create a checkBody middleware
+// Check if body contains the name and price property
+// If not, send back 400 (bad request)
+// Add it to the post handler stack
+
 // 3) ROUTE
 router
   .route('/')
   .get(tourController.getAllTours)
-  .post(tourController.createTour);
+  .post(tourController.checkBody, tourController.checkID);
 
 router
   .route('/:id')
