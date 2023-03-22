f3addcb0ed9710fcd868bbad35c429aac88f3ad4
Cheng-Yuan Lai <a186235@gmail.com>
2023-03-22 23:13:22 +0800

    feat: add checkID middleware

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -5,8 +5,18 @@ const tours = JSON.parse(
 );
 
 // ROUTE HANDLER
+exports.checkID = (req, res, next, val) => {
+  console.log(`Tour id is: ${val}`);
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  next();
+};
+
 exports.getAllTours = (req, res) => {
-  console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
     requestedAt: req.requestTime,
@@ -21,13 +31,7 @@ exports.getTour = (req, res) => {
   console.log(req.params);
   const id = req.params.id * 1; // convert string to number
   const tour = tours.find((el) => el.id === id);
-  // if tour is undefined(not found)
-  if (!tour) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
+
   res.status(200).json({
     status: 'success',
     data: {
@@ -56,13 +60,6 @@ exports.createTour = (req, res) => {
 };
 
 exports.updateTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-
   res.status(200).json({
     status: 'success',
     data: {
@@ -72,13 +69,6 @@ exports.updateTour = (req, res) => {
 };
 
 exports.deleteTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-
   res.status(204).json({
     status: 'success',
     data: null,
