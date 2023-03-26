cbaa1b1e9ecd64f929423d42e3234e860b18b445
Cheng-Yuan Lai <a186235@gmail.com>
2023-03-27 00:29:44 +0800

    refactor: complete the mpdel of the tours

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -79,7 +79,7 @@ exports.createTour = async (req, res) => {
     const newTour = await Tour.create(req.body);
 
     res.status(201).json({
-      status: 'success',
+      status: 'success!!',
       data: {
         tour: newTour,
       },
@@ -87,7 +87,7 @@ exports.createTour = async (req, res) => {
   } catch (err) {
     res.status(400).json({
       status: 'fail',
-      message: 'Invalid data sent!',
+      message: err,
     });
   }
 };
--- models/tourModels.js
+++ models/tourModels.js
@@ -5,15 +5,56 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, 'A tour must have a name'],
     unique: true,
+    trim: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5,
+  },
+  ratingQuanity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'A tour must have a price'],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    // remove all the white space in the beginning and in the end of the string
+    trim: true,
+    required: [true, 'A tour must have a description'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  // We simply leave the images somewhere in the file system
+  // and then put the name of the image itself
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image'],
+  },
+  // save images' name as an array of strings.
+  images: [String],
+  createAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
