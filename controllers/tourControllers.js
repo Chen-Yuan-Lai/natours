b3cf2f592f1320a5af6a5726ec68948adcd40180
Cheng-Yuan Lai <a186235@gmail.com>
2023-03-28 22:09:39 +0800

    refactor: refactoring API apiFeatures
    1. create APIFeatures class
    2. put the code in apiFeatures.js in utils folder

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -1,36 +1,5 @@
 const Tour = require('../models/tourModel');
-
-/* Practice, Show how middleware work
-const fs = require('fs');
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
-
-exports.checkID = (req, res, next, val) => {
-  console.log(`Tour id is: ${val}`);
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-  next();
-};
-
-// Create a checkBody middleware
-exports.checkBody = (req, res, next) => {
-  // Check if body contains the name and price property
-  // If not, send back 400 (bad request)
-  // Add it to the post handler stack
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
-      status: 'fail',
-      message: 'Missing name or price',
-    });
-  }
-  next();
-};
-*/
+const APIFeatures = require('../utils/apiFeatures');
 
 // ROUTE HANDLER
 exports.aliasTopTours = (req, res, next) => {
@@ -42,59 +11,15 @@ exports.aliasTopTours = (req, res, next) => {
 
 exports.getAllTours = async (req, res) => {
   try {
-    // BUILD QUERY
-    // 1A) Filtering
-    // create a shallow copy of query object
-    const queryObj = { ...req.query };
-    const excludedFields = ['page', 'sort', 'limit', 'fields'];
-    excludedFields.forEach((el) => delete queryObj[el]);
-
-    // 1B) Advanced filtering
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    console.log(JSON.parse(queryStr));
-
-    let query = Tour.find(JSON.parse(queryStr));
-
-    // 2) Sorting
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      query = query.sort(sortBy);
-      // sort('price' 'ratingsAverage')
-    } else {
-      query = query.sort('-createdAt');
-    }
-
-    // 3) Field limiting
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields);
-    } else {
-      query = query.select('-__v');
-    }
-
-    // 4) Pagination
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-
-    query = query.skip(skip).limit(limit);
-
-    if (req.query.page) {
-      // get numbers of documents
-      const numTours = await Tour.countDocuments();
-      if (skip >= numTours) throw new Error('This page does not exist');
-    }
-    // mogodb query using operator
-    // { difficulty: 'easy', duration: { $gte: 5} }
-    // http query we extract
-    // { difficulty: 'easy', duration: { gte: 5} }
-
     // EXECUTE QUERY
     // use await to wait until query is already done
-
     // await will try to unwrap any thenables (ex. query oibject)
-    const tours = await query;
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const tours = await features.query;
     // SEND RESPONSE
     res.status(200).json({
       status: 'success',
@@ -103,23 +28,6 @@ exports.getAllTours = async (req, res) => {
         tours,
       },
     });
-    ////////////////////////////////////////////////////
-    // Two ways to write database queries
-    // way 1
-    // const tours = await Tour.find({
-    //   duration: 5,
-    //   difficulty: 'easy',
-    // });
-
-    // using query object
-    // don't use await because we may conduct a bunch of query mathods
-
-    // way 2
-    // const tours = Tour.find()
-    //   .where('duration')
-    //   .equals(5)
-    //   .where('difficulty')
-    //   .equals('easy');
   } catch (err) {
     res.status(404).json({
       status: 'fail',
@@ -206,3 +114,59 @@ exports.deleteTour = async (req, res) => {
     });
   }
 };
+
+/* Practice, show how middleware work
+const fs = require('fs');
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+);
+
+exports.checkID = (req, res, next, val) => {
+  console.log(`Tour id is: ${val}`);
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  next();
+};
+
+// Create a checkBody middleware
+exports.checkBody = (req, res, next) => {
+  // Check if body contains the name and price property
+  // If not, send back 400 (bad request)
+  // Add it to the post handler stack
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Missing name or price',
+    });
+  }
+  next();
+};
+*/
+
+/* Pratice databese query
+mogodb query using operator
+{ difficulty: 'easy', duration: { $gte: 5} }
+http query we extract
+{ difficulty: 'easy', duration: { gte: 5} }
+
+Two ways to write database queries
+way 1
+const tours = await Tour.find({
+  duration: 5,
+  difficulty: 'easy',
+});
+
+using query object
+don't use await because we may conduct a bunch of query mathods
+
+way 2
+const tours = Tour.find()
+  .where('duration')
+  .equals(5)
+  .where('difficulty')
+  .equals('easy');
+*/
---
+++ utils/apiFeatures.js
@@ -0,0 +1,59 @@
+// BUILD QUERIES
+class APIFeatures {
+  constructor(query, queryString) {
+    this.query = query;
+    this.queryString = queryString;
+  }
+
+  filter() {
+    // 1A) Filtering
+    // create a shallow copy of query object
+    const queryObj = { ...this.queryString };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    // 1B) Advanced filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    this.query.find(JSON.parse(queryStr));
+    // return entire object to chain other method
+    return this;
+  }
+
+  // 2) Sorting
+  sort() {
+    if (this.queryString.sort) {
+      const sortBy = this.queryString.sort.split(',').join(' ');
+      // become sort('price' 'ratingsAverage')
+      this.query = this.query.sort(sortBy);
+    } else {
+      this.query = this.query.sort('-createdAt');
+    }
+    return this;
+  }
+
+  // 3) Field limiting
+  limitFields() {
+    if (this.queryString.fields) {
+      const fields = this.queryString.fields.split(',').join(' ');
+      this.query = this.query.select(fields);
+    } else {
+      this.query = this.query.select('-__v');
+    }
+    return this;
+  }
+
+  // 4) Pagination
+  paginate() {
+    const page = this.queryString.page * 1 || 1;
+    const limit = this.queryString.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    this.query = this.query.skip(skip).limit(limit);
+
+    return this;
+  }
+}
+
+module.exports = APIFeatures;
