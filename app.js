107d20012cc29c8aca29abbad8bf6253adc70efa
Cheng-Yuan Lai <a186235@gmail.com>
2023-04-03 13:22:31 +0800

    refactor: move error handler from app.js to errorController.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 
 const AppError = require('./utils/appError');
+const globalErrorhandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -22,30 +23,14 @@ app.use('/api/v1/users', userRouter);
 
 // handle other routes
 app.all('*', (req, res, next) => {
-  // res.status(404).json({
-  //   status: 'faile',
-  //   message: `Can't find ${req.originalUrl} on this server!`,
-  // });
-
-  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
-  err.status = 'faile';
-  err.statusCode = 404;
   // if the next function receives an argument, no matter what it is,
   // Express will automatically know that there was an error
   // then skip all the other middlewares in the middleware
   // stack and sent the error that we passed in
   // to our global error handling middleware,
-  next(err);
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
 // error handling middleware
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalErrorhandler);
 module.exports = app;
