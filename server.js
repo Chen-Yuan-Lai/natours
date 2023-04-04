e9bd06d7cee64e527a14ff6566f02b6a763373f0
Cheng-Yuan Lai <a186235@gmail.com>
2023-04-04 17:28:35 +0800

    feat: handling unhandled rejections ouside Express

--- server.js
+++ server.js
@@ -17,12 +17,30 @@ mongoose
     useFindAndModify: false,
     useUnifiedTopology: true,
   })
-  .then(() => {
-    console.log('DB connection success!');
-  });
+  .then(() => console.log('DB connection success!'));
 
 // START SERVER
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLE REJECTION! Shutting down...');
+
+  // by doing server.close,  we give the server,
+  // basically time to finish all the request
+  // that are still pending or being handled at the time,
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+/*
+each time that there is an unhandled rejection
+somewhere in our application, the process object
+will emit an object called unhandled rejection
+and so we can subscribe to that event by creating
+another listener 
+*/
