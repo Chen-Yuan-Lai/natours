630f6409f1fd63cf6a754cfff44997560ce03be2
Cheng-Yuan Lai <a186235@gmail.com>
2023-04-04 16:51:47 +0800

    feat: Handling Mongoose validation errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -12,6 +12,13 @@ const handleDeuplicateFieldsDB = (err) => {
   return new AppError(message, 400);
 };
 
+const handleValidationErrorDB = (err) => {
+  // extract each message from values of err.errors
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -53,6 +60,8 @@ module.exports = (err, req, res, next) => {
     // handling mongodb invalid ID
     if (error.name === 'CastError') error = handleCastErrorDB(error);
     if (error.code === 11000) error = handleDeuplicateFieldsDB(error);
+    if (error.name === 'ValidationError')
+      error = handleValidationErrorDB(error);
     sendErrorProd(error, res);
   }
 };
