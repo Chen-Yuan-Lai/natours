d7a8c2978198c62864252c7e4e4d1907a4b43f59
Cheng-Yuan Lai <a186235@gmail.com>
2023-04-14 11:30:03 +0800

    feat: Sending JWT via cookie

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -13,8 +13,21 @@ const signToken = (id) => {
 };
 
 const createSendToken = (user, statusCode, res) => {
-  const token = signToken(user.id);
   // create a JWT token
+  const token = signToken(user.id);
+
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOny: true, // the cookie cannot be accessed or modified in any way by the browser.
+  };
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+  // create a cookie
+  res.cookie('jwt', token, cookieOptions);
+
+  // Remove password from the output
+  user.password = undefined;
   res.status(statusCode).json({
     status: 'success',
     token,
