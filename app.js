48a4fce42cfb38a4068b4294db31c82f80a572ce
Cheng-Yuan Lai <a186235@gmail.com>
2023-04-14 11:45:55 +0800

    feat: Implementing rate limiting

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorhandler = require('./controllers/errorController');
@@ -7,11 +9,23 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
-// 1) MIDDLEWARES
-// use middleware
+// 1) GLOBAL MIDDLEWARES
+// Set security HTTP headers
+app.use(helmet());
+
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+// Limit requests from same API
+const limiter = rateLimit({
+  // 100 request per hour
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again hour!',
+});
+app.use('/api', limiter);
+
 app.use(express.json());
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
