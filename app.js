313810c36ce936e08d53c2f6e2d46800503c71fa
Cheng-Yuan Lai <a186235@gmail.com>
2023-04-24 23:12:50 +0800

    feat: Building tour overview and tour page

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const globalErrorhandler = require('./controllers/errorControllers');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
 
@@ -75,25 +76,7 @@ app.use((req, res, next) => {
 
 // 3) Mounting ROUTES
 // mounting the router
-app.get('/', (req, res) => {
-  res.status(200).render('base', {
-    tour: 'The Forest Hiker',
-    user: 'Jonas',
-  });
-});
-
-app.get('/overview', (req, res) => {
-  res.status(200).render('overview', {
-    title: 'All tour',
-  });
-});
-
-app.get('/tour', (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker Tour',
-  });
-});
-
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
